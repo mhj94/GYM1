c3eb9f136bd8361b75dbc434bf3d7e6016b5128a
junlazy <juntae0918@naver.com>
2022-06-15 09:54:47 +0900

    리뷰 별점 미완성

---
+++ src/main/java/com/gym/GYM/company/dao/CommentDAO.java
@@ -0,0 +1,17 @@
+package com.gym.GYM.company.dao;
+
+import com.gym.GYM.company.dto.CommentDTO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface CommentDAO {
+    List<CommentDTO> commentList(String reviewCompanyCode);
+
+    int commentRegist(CommentDTO comment);
+
+    int commentModify(CommentDTO comment);
+
+    int commentDelete(CommentDTO comment);
+}
