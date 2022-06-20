49acee21af92f02ffb6cd325eb205096b097ae56
박건우 <nmb3322>
2022-06-20 09:34:01 +0900

    22.06.20 wishDTO 수정

--- src/main/java/com/gym/GYM/shopping/dto/WishDTO.java
+++ src/main/java/com/gym/GYM/shopping/dto/WishDTO.java
@@ -6,71 +6,47 @@ import org.apache.ibatis.type.Alias;
 public class WishDTO {
 
     /*
-        CREATE TABLE WISH(
-        WISHID               NVARCHAR2(10),  -- 찜한목록아이디
-        WISHMORNING          NVARCHAR2(6),   -- 아침
-        WISHLAUNCH           NVARCHAR2(6),   -- 점심
-        WISHDINNER           NVARCHAR2(6),   -- 저녁
-        WISHMORNINGSNACK     NVARCHAR2(6),   -- 아침간식
-        WISHLAUNCHSNACK      NVARCHAR2(6),   -- 점심간식
-        WISHDINNERSNACK      NVARCHAR2(6),   -- 저녁간식
-     */
+        -- WISH : 찜한목록 테이블
+			CREATE TABLE WISH(
+			WISHID				NVARCHAR2(6),			-- FK MEMBERID
+			WISHCODE			NVARCHAR2(6),			-- FK MYMEALCODE 컬럼6개
+			WISHPRODUCTCODE		NVARCHAR2(6)			-- FK PRODUCT(PRODUCTCODE)
+);*/
 
-    private String wishId;
-    private String wishMorning;
-    private String wishLaunch;
-    private String wishDinner;
-    private String wishMorningSnack;
-    private String wishLaunchSnack;
-    private String wishDinnerSnack;
+        private String wishId;
+		private String wishCode;
+		private String wishProductCode;
 
 	public String getWishId() {
 		return wishId;
 	}
+
 	public void setWishId(String wishId) {
 		this.wishId = wishId;
 	}
-	public String getWishMorning() {
-		return wishMorning;
-	}
-	public void setWishMorning(String wishMorning) {
-		this.wishMorning = wishMorning;
-	}
-	public String getWishLaunch() {
-		return wishLaunch;
-	}
-	public void setWishLaunch(String wishLaunch) {
-		this.wishLaunch = wishLaunch;
-	}
-	public String getWishDinner() {
-		return wishDinner;
-	}
-	public void setWishDinner(String wishDinner) {
-		this.wishDinner = wishDinner;
-	}
-	public String getWishMorningSnack() {
-		return wishMorningSnack;
-	}
-	public void setWishMorningSnack(String wishMorningSnack) {
-		this.wishMorningSnack = wishMorningSnack;
-	}
-	public String getWishLaunchSnack() {
-		return wishLaunchSnack;
-	}
-	public void setWishLaunchSnack(String wishLaunchSnack) {
-		this.wishLaunchSnack = wishLaunchSnack;
-	}
-	public String getWishDinnerSnack() {
-		return wishDinnerSnack;
-	}
-	public void setWishDinnerSnack(String wishDinnerSnack) {
-		this.wishDinnerSnack = wishDinnerSnack;
-	}
-	@Override
-	public String toString() {
-		return "WishDTO [wishId=" + wishId + ", wishMorning=" + wishMorning + ", wishLaunch=" + wishLaunch
-				+ ", wishDinner=" + wishDinner + ", wishMorningSnack=" + wishMorningSnack + ", wishLaunchSnack="
-				+ wishLaunchSnack + ", wishDinnerSnack=" + wishDinnerSnack + "]";
+
+	public String getWishCode() {
+		return wishCode;
 	}
 
+	public void setWishCode(String wishCode) {
+		this.wishCode = wishCode;
+	}
+
+	public String getWishProductCode() {
+		return wishProductCode;
+	}
+
+	public void setWishProductCode(String wishProductCode) {
+		this.wishProductCode = wishProductCode;
+	}
+
+	@Override
+	public String toString() {
+		return "WishDTO{" +
+				"wishId='" + wishId + '\'' +
+				", wishCode='" + wishCode + '\'' +
+				", wishProductCode='" + wishProductCode + '\'' +
+				'}';
+	}
 }
